6dfd432c6ee586e1101738eda05bdd2a9a9b3b71
Tylerw7 <t4wettach@gmail.com>
2025-02-20 16:55:00 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+
+// This class creates a new Node for the doubly list
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+
+class DoublyLinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null,
+            prev: null
+        }
+        this.tail = this.head
+        this.length = 1
+    }
+
+    append(value) {
+        const newNode = new Node(value)
+        newNode.prev = this.tail
+        this.tail.next = newNode
+        this.tail = newNode
+        this.length++
+        return this
+    }
+
+    prepend(value) {
+        const newNode = new Node(value)
+        newNode.next = this.head
+        this.head.prev = newNode
+        this.head = newNode
+        this.length++
+        return this
+    }
+
+    insert(index, value) {
+        //check params
+        if (index >= this.length) {
+            return this.append(value)
+        }
+
+        const newNode = new Node(value)
+        const leader = this.traverseToIndex(index - 1)
+        const follower = leader.next
+        leader.next = newNode
+        newNode.next = follower
+        newNode.prev = leader
+        follower.prev = newNode
+        this.length++
+        return this
+
+    }
+
+    traverseToIndex(index) {
+        let counter = 0
+        let currentNode = this.head
+        while (counter != index) {
+            currentNode = currentNode.next
+            counter++
+        }
+        return currentNode
+    }
+
+    remove(index) {
+        const leader = this.traverseToIndex(index - 1)
+        const unwantedNode = leader.next
+        leader.next = unwantedNode.next
+        this.length--
+        return this
+    }
+
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value); // Store value instead of node
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+}
+
+const mylist = new DoublyLinkedList(10)
+mylist.append(5)
+mylist.append(16)
+mylist.prepend(1)
+mylist.insert(2, 99)
+console.log(mylist.printList())
+
+
